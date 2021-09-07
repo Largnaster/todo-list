b2377f93b7f0f1cf8c1096b94dba755829ffd636
Largnaster <40041090+Largnaster@users.noreply.github.com>
2021-09-07 17:50:01 -0500

    La aplicacion se conecta con firebase y recibe datos de ella

--- src/App.tsx
+++ src/App.tsx
@@ -1,122 +1,12 @@
-import React, { useContext, useRef, useState } from "react";
-import { AuthContext } from "./context/AuthContext";
-import { makeStyles } from "@material-ui/core/styles";
-import {
-  Button,
-  ButtonGroup,
-  Container,
-  FormControl,
-  FormGroup,
-  FormHelperText,
-  AppBar,
-  Toolbar,
-  Typography,
-  TextField,
-} from "@material-ui/core";
-import List from "./pages/List";
-import { auth } from "./firebaseSetup";
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
+import React from "react";
+import Login from './pages/Login'
+import { AuthProvider } from "./provider/AuthProvider";
 
 function App() {
-  const user = useContext(AuthContext);
-  const emailRef = useRef<HTMLInputElement>(null);
-  const passRef = useRef<HTMLInputElement>(null);
-  const classes = useStyles();
-  const [err, setErr] = useState(false);
-  const [errMsg, setErrMsg] = useState("");
-
-  const createAccount = async () => {
-    console.log(emailRef.current!.value);
-
-    try {
-      await auth.createUserWithEmailAndPassword(
-        emailRef.current!.value,
-        passRef.current!.value
-      );
-      setErr(false);
-    } catch (error) {
-      console.log(error.message);
-      setErrMsg(error.message);
-      setErr(true);
-    }
-  };
-
-  const signIn = async () => {
-    try {
-      await auth.signInWithEmailAndPassword(
-        emailRef.current!.value,
-        passRef.current!.value
-      );
-      setErr(false);
-    } catch (error) {
-      console.error(error);
-      setErrMsg(error.message);
-      setErr(true);
-    }
-  };
-
-  const signOut = async () => {
-    await auth.signOut();
-  };
-
   return (
-    <>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" className={classes.title}>
-            Todo List
-          </Typography>
-          {user && (
-            <Button onClick={signOut} color="inherit">
-              Log out
-            </Button>
-          )}
-        </Toolbar>
-      </AppBar>
-      {!user ? (
-        <Container maxWidth="sm">
-          <FormGroup>
-            <FormControl>
-              <TextField error={err} inputRef={emailRef} label="Email" />
-              <FormHelperText id="helper">
-                El correo con el que te registraste
-              </FormHelperText>
-            </FormControl>
-            <FormControl>
-              <TextField error={err} inputRef={passRef} label="Password" />
-              <FormHelperText id="helper">
-                No mustres tu contraseña a nadie
-              </FormHelperText>
-            </FormControl>
-            <label style={{ visibility: err ? "visible" : "hidden" }}>
-              {errMsg}
-            </label>
-            <ButtonGroup>
-              <Button
-                onClick={createAccount}
-                color="primary"
-                variant="outlined"
-              >
-                Sign up
-              </Button>
-              <Button onClick={signIn} color="primary" variant="contained">
-                Login
-              </Button>
-            </ButtonGroup>
-          </FormGroup>
-        </Container>
-      ) : (
-        <List />
-      )}
-    </>
+    <AuthProvider>
+      <Login />
+    </AuthProvider>
   );
 }
 
--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,21 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
+import { AuthContext } from "context/AuthContext";
 import { TodoList } from "./TodoList";
 import { AddTodoForm } from "../../AddTodoForm";
+import { Container } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core";
 // Que onda soy un comentario
 
 const initialTodos: Todo[] = [
   {
-    text: "Walk the dog",
+    text: "Add your first thing to do",
     complete: false,
-  },
-  {
-    text: "Write app",
-    complete: true,
-  },
+  }
 ];
+
+const useStyles = makeStyles((theme) => ({
+  content: {
+    marginTop: "7em"
+  }
+}))
 
 function List() {
   const [todos, setTodos] = useState(initialTodos);
+  const [userid, setUserid] = useState('')
+  const classes = useStyles()
+
+  const user = useContext(AuthContext)
 
   const toggleComplete: ToggleComplete = (selectedTodo: Todo) => {
     const newTodos = todos.map((todo) => {
@@ -35,11 +44,47 @@ function List() {
     setTodos([...todos, newTodo]);
   };
 
+  // try get the data from the user
+  const getUserData = async () => {
+    try {
+      if(user){
+        await setUserid(user.uid)
+        await localStorage.setItem("uid", user.uid)
+        localStorage.setItem("token", await user.getIdToken())
+        const uri:string = `https://tests-3d7ec-default-rtdb.firebaseio.com/todo-list/${localStorage.getItem("uid")}.json?auth=${localStorage.getItem("token")}`
+        fetch(uri)
+        .then(res => {
+          if(res.ok){
+            console.log("response ok");
+            return res.json()
+          }
+          throw res
+        })
+        .then((result) => {
+          console.log("-- RESULTADOS DEL FETCH --\n",result,"\n-- LO QUE TENGO EN EL TODO--\n",todos)
+          setTodos([...todos,result])
+          console.log("\n-- NUEVO TODOS --\n",todos)
+        }).catch(error => {
+          console.error("Error obteniendo los datos: ", error)
+        })
+      }
+      
+      return
+    } catch (e) {
+      const error:Error = e
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    getUserData()
+  }, [])
+
   return (
-    <div>
+    <Container className={classes.content} maxWidth="sm">
       <TodoList todos={todos} toggleComplete={toggleComplete} />
       <AddTodoForm addTodo={addTodo} />
-    </div>
+    </Container>
   );
 }
 
